bd864c9089f240f07140054e02f2a9ea8a187257
~TʜᴇFʟᴀᴍᴇX <138563418+V-E-N-O-X@users.noreply.github.com>
2024-05-09 08:47:29 +0600

    ᴄᴏᴍᴘʟᴇᴛᴇ 100 sᴛᴀʀs ⭐

--- iris_plugins/mention.js
+++ iris_plugins/mention.js
@@ -1,3 +1,48 @@
+//══════════════════════════════════════════════════════════════════════════════════════════════════════// 
+//                                                                                                      //
+//                                     LIGHT-WEIGHT WHATSAPP BOT                                        //
+//                                                                                                      //
+//                                             𝚅.1.2.7                                                  // 
+//                                                                                                      //
+//                          ██╗ ██████╗ ██╗███████╗    ███╗   ███╗ ██████╗                              //
+//                          ██║ ██╔══██╗██║██╔════╝    ████╗ ████║ ██╔══██╗                             //
+//                          ██║ ██████╔╝██║███████╗    ██╔████╔██║ ██║  ██║                             //
+//                          ██║ ██╔══██╗██║╚════██║    ██║╚██╔╝██║ ██║  ██║                             //
+//                          ██║ ██║  ██║██║███████║    ██║ ╚═╝ ██║ ██████╔╝                             //
+//                          ╚═╝ ╚═╝  ╚═╝╚═╝╚══════╝    ╚═╝     ╚═╝ ╚═════╝                              //
+//                                                                                                      //
+//                                          BY: VENOX-SENPAI                                            //
+//                                                                                                      //
+//                                                                                                      //
+//══════════════════════════════════════════════════════════════════════════════════════════════════════//
+
+/**
+
+* @project_name : IRIS-MD
+* @Developer : Venox-Senpai
+* @Version : v.1.2.7
+* @license : APACHE-2.0
+
+THIS PROJECT IS MADE UNDER THE CIRCUMSTANCES OF "APACHE-2.0"
+MEANS ANY KIND OF (MISLEADING, RE-UPLOADING, RENG) WILL LEAD YOU
+TO A DMCA REPORT FROM THE OFFICIAL GITHUB, WHICH RESULT IN 
+REMOVING UR COPIED/RE-UPLOADED PROJECT FROM THERE, IN SOME
+CONDITIONS IT CAN LEADS TO TERMINATE UR ACCOUNT FROM THAT PLATFORM
+
+│• @C_holder : Venox-Senpai
+
+**/
+
+
+
+
+
+
+
+
+
+
+
 const { INFO, SUDO, IG } = require('../info.js');
 const { iris, isPublic } = require("../lib/commands.js");
 const { toAudio, getBuffer } = require("../lib/functions.js");
